a54c727c246c8af43beb06b93766711e4e9f0f92
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-03-03 20:12:15 +0530

    feat: add swagger ui

--- Program.cs
+++ Program.cs
@@ -1,5 +1,25 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "OpenChat API",
+        Description = "REST API for OpenChat",
+        Version = "v1"
+    });
+});
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "OpenChat API V1");
+});
 
 app.MapGet("/", () => "Hello World!");
 
