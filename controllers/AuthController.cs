0f5a8f502c366569e78060effb01751ac6f4ef55
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-03-04 23:26:26 +0530

    feat: generate password hash & salt

---
+++ controllers/AuthController.cs
@@ -0,0 +1,35 @@
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using Chat.User;
+using Chat.UserDto;
+
+namespace OpenChat.Auth.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/auth")]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+
+        [HttpPost("/register")]
+        public Task<ActionResult<User>> Register(UserDto request)
+        {
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+            user.Username = request.Username;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+            return Task.FromResult<ActionResult<User>>(new ActionResult<User>(user));
+        }
+
+        // create a password hash and salt
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
