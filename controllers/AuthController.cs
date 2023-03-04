5c7877a46df186a26e95e126c16c25873c5f8615
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-03-05 00:14:56 +0530

    feat: setup login route

--- controllers/AuthController.cs
+++ controllers/AuthController.cs
@@ -16,10 +16,26 @@ namespace OpenChat.Auth.Controllers
         public Task<ActionResult<User>> Register(UserDto request)
         {
             CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
             user.Username = request.Username;
             user.PasswordHash = passwordHash;
             user.PasswordSalt = passwordSalt;
+
             return Task.FromResult<ActionResult<User>>(new ActionResult<User>(user));
+        }
+
+        [HttpPost("login")]
+        public Task<ActionResult<string>> Login(UserDto request)
+        {
+            if (user.Username == request.Username)
+            {
+                if (VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+                {
+                    return Task.FromResult<ActionResult<string>>(new ActionResult<string>("User logged in"));
+                }
+            }
+
+            return Task.FromResult<ActionResult<string>>(new ActionResult<string>("User not found"));
         }
 
         // create a password hash and salt
@@ -29,6 +45,16 @@ namespace OpenChat.Auth.Controllers
             {
                 passwordSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        // verify password hash and salt
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(user.PasswordHash);
             }
         }
     }
